3cd469c361bc6c918415a7e5c519823ebb04d628
Ken-Yi Lee <kenyilee@gmail.com>
2019-05-12 20:53:56 +0800

    Update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ async function main () {
   server.listen(port)
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.setViewport({ width: 1280, height: 800 })
+//  await page.setViewport({ width: 1280, height: 800 })
   await page.goto(`http://127.0.0.1:${port}`)
   await page.screenshot({ path: 'test.png', fullpage: true })
   await browser.close()
