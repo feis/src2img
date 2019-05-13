83361c3a5840378789114fd089b37769aa80a40e
Ken-Yi Lee <kenyilee@gmail.com>
2019-05-13 09:31:30 +0800

    Remove the requirement of running a http server

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const http = require('http')
-const httpShutdown = require('http-shutdown')
 const puppeteer = require('puppeteer')
 const fs = require('fs')
 const prismjs = require('prismjs')
@@ -8,47 +6,25 @@ const { template } = require('lodash')
 
 loadPrismLanguages(['cpp'])
 
-function cssRequestHandler (res) {
-  res.end(
-    fs.readFileSync(
-      require.resolve('prismjs/themes/prism.css')
-    )
-  )
-}
+async function main () {
+  const outputFilename = 'test.png'
 
-async function rootRequestHandler (req, res) {
-  if (req.url === '/themes/prism.css') {
-    cssRequestHandler(res)
-    return
-  }
+  const css =
+    fs.readFileSync(
+      require.resolve('prismjs/themes/prism.css'))
 
   const src = fs.readFileSync(process.argv[process.argv.length - 1], 'utf8')
   const highlighted = prismjs.highlight(src, prismjs.languages.cpp, 'cpp')
-  res.end(template(fs.readFileSync('template.html', 'utf8'))({ highlighted }))
-}
-
-async function main () {
-  const outputFilename = 'test.png'
-  const port = 3000
-
-  const server = httpShutdown(http.createServer(rootRequestHandler))
-  server.listen(port)
 
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto(`http://127.0.0.1:${port}`)
+  await page.setContent(
+    template(fs.readFileSync('template.html', 'utf8'))({
+      css,
+      highlighted,
+    }))
   await page.screenshot({ path: outputFilename, fullpage: true })
   await browser.close()
-
-  process.on('SIGINT', () => {
-    server.shutdown()
-  })
-
-  if (process.argv.indexOf('--server') !== -1) {
-    console.log('Server mode')
-  } else {
-    process.kill(process.pid, 'SIGINT')
-  }
 }
 
 module.exports = new Promise(main)
