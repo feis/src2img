a0d3b21bcfa07d3c6983061fc0c489b2790409cc
Ken-Yi Lee <kenyilee@gmail.com>
2019-12-03 23:08:12 +0800

    Update

--- index.js
+++ index.js
@@ -26,10 +26,6 @@ function hasParam (paramName) {
   return process.argv.indexOf(paramName) !== -1
 }
 
-function readConfig (configFilename) {
-  return null
-}
-
 async function main () {
   const fakeroot = 'http://127.0.0.1'
 
@@ -38,18 +34,14 @@ async function main () {
   const outputDir = getParam('--output', 'out')
   const configFile = getParam('--config', null)
 
-  const config = readConfig(configFile)
+
 
   const browser = await puppeteer.launch()
 
   console.log('Input directory: ' + inputDir)
 
-  const inputFilenames =
-    fs.readdirSync(inputDir)
-      .filter(e => ['.cpp', '.cc'].includes(path.extname(e)))
-
   const page = await browser.newPage()
-  await page.setViewport({ width: 1920, height: 1 })
+  await page.setViewport({ width: 2000, height: 1 })
   await page.setRequestInterception(true)
 
   let html = ''
@@ -70,24 +62,46 @@ async function main () {
     }
   })
 
-  for (const inputFilename of inputFilenames) {
-    console.log('Processing: ' + inputFilename)
-    const inputFilepath = path.join(inputDir, inputFilename)
-    const truncated = escapeHtmlEntities(fs.readFileSync(inputFilepath, 'utf8')).trim().split('\n')
-    truncated.shift()
-    const src = truncated.join('\n')
-    const startLine = 2
+  const configs =
+    configFile === null ?  
+      fs.readdirSync(inputDir)
+        .filter(e => ['.cpp', '.cc']
+        .includes(path.extname(e)))
+        .map((e) => { return {filename: e}; }) :
+      JSON.parse(fs.readFileSync(configFile, 'utf8'))
+
+  for (const config of configs) {
+    console.log('Processing: ' + config.filename)
+    const inputFilepath = path.join(inputDir, config.filename)
+
+    const startLine = config.startLine || 2
+
+    const lines =
+      escapeHtmlEntities(fs.readFileSync(inputFilepath, 'utf8'))
+        .trim()
+        .split('\n')
+
+    const endLine = config.endLine || lines.length
+
+    const src = lines.slice(startLine - 1, endLine).join('\n')
 
     html = template(fs.readFileSync('template.html', 'utf8'))({ src, startLine })
 
     if (isHtmlEnabled) {
       fs.writeFileSync(
-        path.join(outputDir, inputFilename + '.html'),
-        html)
+        path.join(outputDir, config.filename + '.html'),
+        html,
+        'utf8')
     }
 
     await page.goto(fakeroot)
-    const outputFilepath = path.join(outputDir, inputFilename + '.png')
+
+    const filename = config.filename.replace(/^.*[\\\/]/, '')
+
+    const outputFilename = `${filename}.${startLine}-${endLine}.png`
+
+    const outputFilepath = path.join(outputDir, outputFilename)
+
     await page.screenshot({
       path: outputFilepath,
       fullPage: true,
