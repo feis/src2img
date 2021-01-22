c96d7850aeffe92aebb180b82cee7d34b66ed701
Ken-Yi Feis Lee <feislee@sigono.com>
2021-01-23 01:21:13 +0800

    Update

--- index.ts
+++ index.ts
@@ -3,42 +3,45 @@ const fs = require('fs')
 const { template } = require('lodash')
 const path = require('path')
 
-var tagsToReplace = {
+const tagsToReplace : {[tag: string]: string} = {
   '&': '&amp;',
   '<': '&lt;',
   '>': '&gt;'
 }
 
-function replaceTag (tag) {
+function replaceTag (tag: string) : string {
   return tagsToReplace[tag] || tag
 }
 
-function escapeHtmlEntities (str) {
+function escapeHtmlEntities (str: string) {
   return str.replace(/[&<>]/g, replaceTag)
 }
 
-function getParam (paramName, defaultValue) {
+function getParam (paramName: string, defaultValue: string|null) {
   const index = process.argv.indexOf(paramName)
   return index === -1 ? defaultValue : process.argv[ index + 1 ]
 }
 
-function hasParam (paramName) {
+function hasParam (paramName: string) {
   return process.argv.indexOf(paramName) !== -1
 }
 
 async function main () {
-  const fakeroot = 'http://127.0.0.1'
+  const fakeRoot = 'http://127.0.0.1'
 
   const isHtmlEnabled = hasParam('--html')
-  const inputDir = getParam('--input', 'in')
-  const outputDir = getParam('--output', 'out')
-  const configFile = getParam('--config', null)
-
-
+  const configFile = getParam('--config', 'config.json')
 
   const browser = await puppeteer.launch()
 
-  console.log('Input directory: ' + inputDir)
+  const configs : {
+    input: string,
+    output: string,
+    language: "cpp"|"python",
+    sources: any[],
+  } = JSON.parse(fs.readFileSync(configFile, 'utf8'))
+
+  console.log('Input directory: ' + configs.input)
 
   const page = await browser.newPage()
   await page.setViewport({ width: 2000, height: 1 })
@@ -46,8 +49,8 @@ async function main () {
 
   let html = ''
 
-  page.on('request', request => {
-    const fileRelativePath = request.url().substr(fakeroot.length)
+  page.on('request', (request: any) => {
+    const fileRelativePath = request.url().substr(fakeRoot.length)
     if (fileRelativePath === '/') {
       request.respond({ body: html })
     } else if (fileRelativePath === '/prism.css') {
@@ -55,52 +58,45 @@ async function main () {
     } else {
       request.respond({
         body:
-          fs.readFileSync(
-            path.join(
-              path.dirname(require.resolve('prismjs')),
-              fileRelativePath)) })
+            fs.readFileSync(
+              path.join(
+                path.dirname(require.resolve('prismjs')),
+                fileRelativePath)) })
     }
   })
 
-  const configs =
-    configFile === null ?  
-      fs.readdirSync(inputDir)
-        .filter(e => ['.cpp', '.cc']
-        .includes(path.extname(e)))
-        .map((e) => { return {filename: e}; }) :
-      JSON.parse(fs.readFileSync(configFile, 'utf8'))
 
-  for (const config of configs) {
+
+  for (const config of configs.sources) {
     console.log('Processing: ' + config.filename)
-    const inputFilepath = path.join(inputDir, config.filename)
+    const inputFilepath = path.join(configs.input, config.filename)
 
     const startLine = config.startLine || 2
 
     const lines =
-      escapeHtmlEntities(fs.readFileSync(inputFilepath, 'utf8'))
-        .trim()
-        .split('\n')
+        escapeHtmlEntities(fs.readFileSync(inputFilepath, 'utf8'))
+          .trim()
+          .split('\n')
 
     const endLine = config.endLine || lines.length
 
     const src = lines.slice(startLine - 1, endLine).join('\n')
 
-    html = template(fs.readFileSync('template.html', 'utf8'))({ src, startLine })
+    html = template(fs.readFileSync('template_'+configs.language+'.html', 'utf8'))({ src, startLine })
 
     if (isHtmlEnabled) {
       fs.writeFileSync(
-        path.join(outputDir, config.filename + '.html'),
+        path.join(config.output, config.filename + '.html'),
         html,
         'utf8')
     }
 
-    await page.goto(fakeroot)
+    await page.goto(fakeRoot)
 
-    const filename = config.filename.replace(/^.*[\\\/]/, '')
-
+    let filename = config.filename.replace(/^.*[\\\/]/, '');
     const outputFilename = `${filename}.${startLine}-${endLine}.png`
 
-    const outputFilepath = path.join(outputDir, outputFilename)
+    const outputFilepath = path.join(configs.output, outputFilename)
 
     await page.screenshot({
       path: outputFilepath,
